43a8340d532f52d166cd3127304b06c707c2b9ad
mohit22500 <mohitkumarchoudhary17@gmail.com>
2025-09-24 00:35:49 +0530

    Third commit

--- app/src/main/java/org/example/services/UserBookingService.java
+++ app/src/main/java/org/example/services/UserBookingService.java
@@ -7,6 +7,7 @@ import org.example.entities.User;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 public class UserBookingService {
     private User user;
@@ -15,12 +16,31 @@ public class UserBookingService {
 
     private ObjectMapper objectMapper = new ObjectMapper();
 
-    private static final String USERS_PATH="../localDB/Users.json";
+    private static final String USERS_PATH="app/src/main/java/org/example/localDB/users.json";
 
     public UserBookingService(User user1) throws IOException {
         this.user=user1;
         File users=new File(USERS_PATH);
-        userList=objectMapper.readValue(Users, new TypeReference<List<User>>() {});
+        userList=objectMapper.readValue(users, new TypeReference<List<User>>() {});
+    }
+
+    public Boolean loginUser(){
+        Optional<User> foundUser=userList.stream().filter(user1->{
+            return user1.getName().equals(user.getName()) && UserServiceUtil.checkPassword(user.getPassword(),user1.getHashedPassword());
+        }).findFirst();
+
+        return foundUser.isPresent();
+    }
+
+    public Boolean signUp(User user){
+        try{
+            userList.add(user1);
+            saveUserListToFile();
+            return Boolean.TRUE;
+        }
+        catch(IOException ex){
+            return Boolean.FALSE;
+        }
     }
 
 
