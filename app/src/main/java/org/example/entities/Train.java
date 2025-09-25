5c846caff86af5587ac9c2be0a3952ffa7acf938
mohit22500 <mohitkumarchoudhary17@gmail.com>
2025-09-25 10:24:36 +0530

    Update User,Ticket,Train

--- app/src/main/java/org/example/entities/Train.java
+++ app/src/main/java/org/example/entities/Train.java
@@ -9,9 +9,71 @@ public class Train {
 
     private String trainNumber;
 
-    private List<List<Integer>> Seats;
+    private List<List<Integer>> seats;
 
-    private Map<String, Time> stationTimes;
+    private Map<String, String> stationTimes;
 
     private List<String> stations;
+
+    public Train(){};
+    public Train(String trainId, String trainNumber, List<List<Integer>> seats, Map<String, String> stationTimes, List<String> stations)
+    {
+        this.trainId=trainId;
+        this.trainNumber=trainNumber;
+        this.seats=seats;
+        this.stationTimes=stationTimes;
+        this.stations=stations;
+    }
+    public List<String> getStations()
+    {
+        return stations;
+    }
+    public void setStations(List<String> stations)
+    {
+        this.stations=stations;
+    }
+
+    public List<List<Integer>> getSeats()
+    {
+        return seats;
+    }
+    public void setSeats(List<List<Integer>> seats)
+    {
+        this.seats=seats;
+    }
+
+    public String getTrainId()
+    {
+        return trainId;
+    }
+    public void setTrainId(String trainId)
+    {
+        this.trainId=trainId;
+    }
+
+    public String getTrainNumber()
+    {
+        return trainNumber;
+    }
+    public void setTrainNumber(String trainNumber)
+    {
+        this.trainNumber=trainNumber;
+    }
+
+    public Map<String,String> getStationTimes()
+    {
+        return stationTimes;
+    }
+    public void setStationTimes(Map<String,String> stationTimes)
+    {
+        this.stationTimes=stationTimes;
+    }
+
+    public String getTrainInfo()
+    {
+        return String.format("Train ID: %s Train Number: %s",trainId,trainNumber);
+    }
+
+
+
 }
